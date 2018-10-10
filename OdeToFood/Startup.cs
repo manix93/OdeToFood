46d4d4a675b7ed8e9ba3ca1d3f5825e9106cf242
Lukasz Maciag <manix93@gmail.com>
2018-10-10 13:27:33 +0200

    Add EF to project

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -5,17 +5,29 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OdeToFood.Services;
+using OdeToFood.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace OdeToFood
 {
 	public class Startup
 	{
+		private IConfiguration _configuration;
+
+		public Startup(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddSingleton<IGreeter, Greeter>();
-			services.AddSingleton<IRestaurantData, InMemoryRestaurantData>(); //Scoped telling us that if we have IRestaurantData item put there InMemoryRestaurantData class
+			services.AddDbContext<OdeToFoodDbContext>(
+				options => options.UseSqlServer(_configuration.GetConnectionString("OdeToFood")));
+			services.AddScoped<IRestaurantData, SqlRestaurantData>(); //Scoped telling us that if we have IRestaurantData item put there InMemoryRestaurantData class
 			services.AddMvc();
 		}
 
